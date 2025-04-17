4584179759a15e4b7f468598ff6e5d9e87b30a93
ABDULLAHDUWIPUTRO <abdullahduwiputro69@gmail.com>
2025-04-17 10:42:12 +0700

    menambahkan slot panjang array dan maksimal panjang array

--- merge_sort.cpp
+++ merge_sort.cpp
@@ -6,7 +6,21 @@ int arr[20], B[20];
 //n is array input size 
 int n;
 
+void input()
+{
+    while(true)
+    {
+        cout << "masukan panjang element array : ";
+        cin >> n;
 
-
+        if (n <= 20)
+        {   
+            break;
+        }
+        else
+        {
+            cout << "\nMaksimal panjang array adalah 20";
+        }
+ }
 
     
